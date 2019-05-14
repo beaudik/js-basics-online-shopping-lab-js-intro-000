a4f8dc9026a197f2a20c9529621c76aad758cc7b
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-05-14 02:19:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  //var array1 = ['a', 'b', 'c'];
+if (!cart.findIndex) return `That item is not in your cart.`
+cart.forEach(function(element) {
+  if (element == item){
+  cart.splice(cart.indexOf(element), 1);
+  return cart
+
+
+
+});
 }
 
 function placeOrder(cardNumber) {
